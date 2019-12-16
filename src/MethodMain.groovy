cd86a6f8bfb2f153d35c860c0aa8807d593722cd
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-16 22:58:01 +0200

    Create first groovy script

--- src/MethodMain.groovy
+++ src/MethodMain.groovy
@@ -10,7 +10,13 @@ def method3(param1 = "default") {
     println "invoke method2 $param1"
 }
 
+static int method4(int param1 = 1) {
+    return param1 * 2
+}
+
 method1()
 method2 "PARAM-01"
 method3()
 method3 "DEFAULT"
+println(method4())
+println method4(7)
