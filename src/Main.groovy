c6f97477f9774d0a495fa3150f286064625e6415
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-12-16 22:29:36 +0200

    Create first groovy script

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,14 +1,13 @@
 println "for:"
 for (int i = 0; i < 3; i++) {
-    println "Hello World ${i}."
+    println "Hello World $i"
 }
 
 println "upto:"
-0.upto(3) {
-    println "upto ${it}"
-}
+0.upto(3) { println "upto $it" }
 
 println "times:"
-3.times {
-    println "times ${it}"
-}
+3.times { println "times $it" }
+
+println "step:"
+0.step(7, 2) { println "step $it" }
